a75ac6f472e358a5eead719ce55d27e40ff33821
Viviana <vsmith53@alamo.edu>
2019-09-28 20:37:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var button = document.querySelector("button");
+
+var mickeyMouse = document.getElementById("mousey");
+
+
+function loadFunc() {
+	document.getElementById("loady").classList.add("done");
+}
+
+document.getElementById("loady").addEventListener("load", loadFunc());
+
+
+mickeyMouse.addEventListener("mouseover", function(){
+	var liTwo = document.getElementById("mousey");
+	liTwo.classList.add("done");
+ 	});
+
+button.addEventListener("click", function(){
+	var liThree = document.getElementById("clicky");
+	liThree.classList.add("done");
+ 	});
+
+
+
